90b4010e22ce72ad06b2100c4a4b2216aa3868ff
Caris Kao2 <coolcariskao@gmail.com>
2021-05-24 06:27:33 +0800

    done

---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- Header -->
+  <header id="header">
+    <div class="inner">
+      <!-- Logo -->
+      <nuxt-link to="/" class="logo">
+        <span class="symbol"><img src="~/assets/images/logo.svg" alt="" /></span
+        ><span class="title">Phantom</span>
+      </nuxt-link>
+
+      <!-- Nav -->
+      <nav>
+        <ul>
+          <li><a href="#menu">Menu</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+<style lang="scss" scoped>
+  #header {
+    .title {
+      background-color: pink;
+    }
+  }
+</style>
